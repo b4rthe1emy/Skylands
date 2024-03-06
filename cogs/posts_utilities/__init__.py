1b56a69b7f5ba20fb8d943f43bac90c1a1f6ecbe
barthelemy.villotte@gmail.com <barthelemy.villotte@gmail.com>
2024-03-06 19:36:19 +0100

    feat: add on thread create

--- cogs/posts_utilities/__init__.py
+++ cogs/posts_utilities/__init__.py
@@ -4,9 +4,12 @@ from rich import print
 import dotenv
 
 SUGESTIONS_FORUM_ID = int(dotenv.get_key(dotenv.find_dotenv(), "SUGESTIONS_FORUM_ID"))
-ENDED_POST_TAG_ID = int(dotenv.get_key(dotenv.find_dotenv(), "ENDED_POST_TAG_ID"))
 ADMIN_ROLE_ID = int(dotenv.get_key(dotenv.find_dotenv(), "ADMIN_ROLE_ID"))
 MEMBER_ROLE_ID = int(dotenv.get_key(dotenv.find_dotenv(), "MEMBER_ROLE_ID"))
+
+WAITING_POST_TAG_ID = int(dotenv.get_key(dotenv.find_dotenv(), "WAITING_POST_TAG_ID"))
+WIP_POST_TAG_ID = int(dotenv.get_key(dotenv.find_dotenv(), "WIP_POST_TAG_ID"))
+ENDED_POST_TAG_ID = int(dotenv.get_key(dotenv.find_dotenv(), "ENDED_POST_TAG_ID"))
 
 
 class PostUtilities(commands.Cog):
@@ -15,6 +18,18 @@ class PostUtilities(commands.Cog):
     def __init__(self, bot) -> None:
         super().__init__()
         self.bot = bot
+
+        @bot.event
+        async def on_thread_create(thread: nextcord.Thread):
+            if (
+                not isinstance(thread.parent, nextcord.ForumChannel)
+                or thread.parent.id != SUGESTIONS_FORUM_ID
+            ):
+                return
+
+            tags = thread.applied_tags.copy()
+            tags.append(thread.parent.get_tag(WAITING_POST_TAG_ID))
+            await thread.edit(applied_tags=tags, locked=True)
 
     @nextcord.slash_command(
         "post",
@@ -48,7 +63,20 @@ class PostUtilities(commands.Cog):
 
         tags = thread.applied_tags.copy()
         tags.append(forum.get_tag(ENDED_POST_TAG_ID))
-        await thread.edit(applied_tags=tags, locked=True)
+        try:
+            tags.remove(forum.get_tag(WAITING_POST_TAG_ID))
+        except ValueError:
+            pass
+        try:
+            tags.remove(forum.get_tag(WIP_POST_TAG_ID))
+        except ValueError:
+            pass
+
+        try:
+            await thread.edit(applied_tags=tags)
+        except nextcord.errors.HTTPException:
+            pass
+        await thread.edit(locked=True)
 
         await interaction.response.send_message("Ce post est terminé.")
 
@@ -75,7 +103,15 @@ class PostUtilities(commands.Cog):
         await messages[0].remove_reaction("✅", self.bot.user)
 
         tags = thread.applied_tags.copy()
-        tags.remove(forum.get_tag(ENDED_POST_TAG_ID))
-        await thread.edit(applied_tags=tags, locked=False)
+        try:
+            tags.remove(forum.get_tag(ENDED_POST_TAG_ID))
+        except ValueError:
+            pass
+        tags.append(forum.get_tag(WIP_POST_TAG_ID))
+        try:
+            await thread.edit(applied_tags=tags)
+        except nextcord.errors.HTTPException:
+            pass
+        await thread.edit(locked=True)
 
         await interaction.response.send_message("Ce post est réactivé.")
