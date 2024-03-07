a9317c617908e5f598ee723010d5c47ea692dcaf
b4rthe1emy <132834665+b4rthe1emy@users.noreply.github.com>
2024-03-07 22:06:25 +0100

    refactor: make beautiful command tree

--- main.py
+++ main.py
@@ -25,12 +25,42 @@ bot.add_cog(PostUtilities(bot))
 bot.add_all_cog_commands()
 
 
+def command_representation(command: nextcord.BaseApplicationCommand, shift: int = 0):
+    command_symbol = {
+        nextcord.application_command.SlashApplicationCommand: "[blue]▐[/blue][white on blue]/[/white on blue][blue]▌[/blue]",
+        nextcord.application_command.SlashApplicationSubcommand: "[blue]▐[/blue][white on blue]/[/white on blue][blue]▌[/blue]",
+        nextcord.application_command.UserApplicationCommand: "[bright_yellow]▐[/bright_yellow][white on bright_yellow]@[/white on bright_yellow][bright_yellow]▌[/bright_yellow]",
+    }
+    output: str = command_symbol[type(command)] + " " + command.name
+    output += (
+        " " * (85 - len(output) - shift)
+        + "[black italic]"
+        + command.description
+        + "[/black italic]"
+    )
+    if (
+        isinstance(command, nextcord.SlashApplicationCommand)
+        and list(command.children.values()) != []
+    ):
+
+        children = list(command.children.values())
+        for i, command in enumerate(children):
+            if i != len(children) - 1:
+                output += "\n ├── " + command_representation(command, shift=5)
+            else:
+                output += "\n ╰── " + command_representation(command, shift=5)
+        output += "\n"
+
+    return output
+
+
 @bot.event
 async def on_ready():
-    print(">> [bold green underline]Bot ready[/bold green underline] <<")
-    print(
-        f"[bold blue]Bot Commands:[/bold blue] [bright_black]{[command.name for command in bot.get_application_commands()]}[/bright_black]"
+    commands = "\n".join(
+        [command_representation(command) for command in bot.get_application_commands()]
     )
+    print(">> [bold green underline]Bot ready[/bold green underline] <<")
+    print(f"[bold blue]Bot Commands:[/bold blue]\n{commands}")
 
 
 if __name__ == "__main__":
