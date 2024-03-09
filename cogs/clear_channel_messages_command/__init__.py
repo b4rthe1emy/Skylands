b4d773b5e1d7b74f7f589b24907add6e40e09dd9
b4rthe1emy <132834665+b4rthe1emy@users.noreply.github.com>
2024-03-09 15:56:46 +0100

    fix: add permission to clear command

--- cogs/clear_channel_messages_command/__init__.py
+++ cogs/clear_channel_messages_command/__init__.py
@@ -4,10 +4,25 @@ from rich import print
 
 
 class ClearChannelMessagesCommand(commands.Cog):
-    @nextcord.slash_command("clear_salon")
-    async def clear_salon(self, interaction: nextcord.Interaction):
-        await interaction.response.send_message(
-            "Tous les messages de ce salon vont être supprimés. Cette opération peut prendre longtemps, merci de patienter.",
-            ephemeral=True,
-        )
-        await interaction.channel.purge()
+    @nextcord.slash_command(
+        "supprimer_les_messages",
+        "ATTENTION: SUPPRIME TOUS LES MESSAGES DU SALON. À UTILISER UNIQUEMENT SI TU SAIS CE QUE TU FAIS!!!",
+        default_member_permissions=nextcord.Permissions(administrator=True),
+    )
+    async def clear_salon(
+        self,
+        interaction: nextcord.Interaction,
+        sure: bool = nextcord.SlashOption(
+            "sûr", "T'ES VRAIMENT SÛR???", default=False, required=False
+        ),
+    ):
+        if sure:
+            await interaction.response.send_message(
+                "Tous les messages de ce salon vont être supprimés. Cette opération peut prendre longtemps, merci de patienter.",
+                ephemeral=True,
+            )
+            await interaction.channel.purge()
+        else:
+            await interaction.response.send_message(
+                'Met l\'option "sûr" à Vrai si tu es vraiment sûr.', ephemeral=True
+            )
