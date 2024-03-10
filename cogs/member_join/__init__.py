de27af27bcd27eb2721c6a9a189ada2b1b363428
b4rthe1emy <132834665+b4rthe1emy@users.noreply.github.com>
2024-03-10 19:51:07 +0100

    feat: add welcome command that does the same thing as when a member joins

--- cogs/member_join/__init__.py
+++ cogs/member_join/__init__.py
@@ -8,7 +8,7 @@ SKYLANDS_GUILD_ID = int(dotenv.get_key(dotenv.find_dotenv(), "SKYLANDS_GUILD_ID"
 MEMBER_ROLE_ID = int(dotenv.get_key(dotenv.find_dotenv(), "MEMBER_ROLE_ID"))
 
 
-class MemberJoin:
+class MemberJoin(commands.Cog):
     bot: nextcord.Client
 
     @property
@@ -19,20 +19,39 @@ class MemberJoin:
     def skylands_guild(self):
         return self.bot.get_guild(SKYLANDS_GUILD_ID)
 
+    async def send_welcome_message(self, member: nextcord.Member):
+        embed = nextcord.Embed(
+            color=0x3498DB,
+            title="**Bienvenue sur Skylands**",
+            url="https://skylandsmc.fr",
+        )
+        embed.add_field(
+            name=f"🛬 Bienvenue **{member.global_name}**, amuse toi bien sur 𝐒𝐤𝐲𝐥𝐚𝐧𝐝𝐬 ! *(tu es le membre #{self.skylands_guild.member_count})*",
+            value="Tu peux lier ton compte Minecraft avec ton compte Discord en tapant la commande `/link` en jeu.",
+        )
+        await self.welcome_channel.send(member.mention, embed=embed)
+
+    @nextcord.slash_command(
+        "welcome",
+        "Envoie le message de bienvenue",
+        default_member_permissions=nextcord.Permissions(administrator=True),
+    )
+    async def welcome(
+        self,
+        interaction: nextcord.Interaction,
+        member: nextcord.Member = nextcord.SlashOption(
+            "utilisateur", "L'utilisateur à souhaiter la bienvenue."
+        ),
+    ):
+        await self.send_welcome_message(member)
+        await interaction.response.send_message(
+            f"Bienvenue souhaitée à {member.mention}.", ephemeral=True
+        )
+
     def __init__(self, bot: nextcord.Client) -> None:
         self.bot: commands.Bot = bot
 
         @self.bot.event
         async def on_member_join(member: nextcord.Member):
-            embed = nextcord.Embed(
-                color=0x3498DB,
-                title="**Bienvenue sur Skylands**",
-                url="https://skylandsmc.fr",
-            )
-            embed.add_field(
-                name=f"🛬 Bienvenue **{member.global_name}**, amuse toi bien sur 𝐒𝐤𝐲𝐥𝐚𝐧𝐝𝐬 ! *(tu es le membre #{self.skylands_guild.member_count})*",
-                value="Tu peux lier ton compte Minecraft avec ton compte Discord en tapant la commande `/link` en jeu.",
-            )
-            await self.welcome_channel.send(member.mention, embed=embed)
-
-            await member.add_roles(self.skylands_guild.get_role(MEMBER_ROLE_ID))
+            await self.send_welcome_message(member)
+            # await member.add_roles(self.skylands_guild.get_role(MEMBER_ROLE_ID))
